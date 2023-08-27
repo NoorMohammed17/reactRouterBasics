ca2c3452ef5eff0f7035d3c1c9505f3b96bcbd14
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-08-27 23:24:35 +0530

    files modified

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import {createBrowserRouter,RouterProvider} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
+import ProductsPage from './pages/ProductsPage';
 //import TodoApp from './TodoProject/TodoApp';
 
 const App = () => {
   const router = createBrowserRouter([
     { path: "/",
-    element: <HomePage />}
+    element: <HomePage />},
+    { path: "/products",
+    element: <ProductsPage />},
   ])
 
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <div>
-        <p>HomePage</p>
-      
+      <h1>HomePage</h1>
+      <p>
+        {" "}
+        Go to <Link to='/products'> the list of products</Link>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
-
+export default HomePage;
